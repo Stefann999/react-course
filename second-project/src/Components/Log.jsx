8e92b5e2b50fe0c3adb088049ee9baa99d2e5399
Stefann999 <stefchog25@gmail.com>
2024-09-10 17:13:14 +0300

    Implement log

--- second-project/src/Components/Log.jsx
+++ second-project/src/Components/Log.jsx
@@ -1,7 +1,13 @@
-export default function Log() {
+export default function Log({ turns }) {
 
 
-    return <ol id="log">
-
-    </ol>
+    return (
+        <ol id="log">
+            {turns.map(turn => (
+                <li key={`${turn.square.row}${turn.square.cow}`}>
+                    {turn.player} selected {turn.square.row}, {turn.square.cow}
+                </li>
+            ))}
+        </ol>
+    );
 }
