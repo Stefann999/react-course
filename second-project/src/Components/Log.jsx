933246ee9ec1b259f6ac6fe8b2a41eba9a912080
Stefann999 <stefchog25@gmail.com>
2024-09-10 17:43:12 +0300

    Fix log outputting

--- second-project/src/Components/Log.jsx
+++ second-project/src/Components/Log.jsx
@@ -3,9 +3,9 @@ export default function Log({ turns }) {
 
     return (
         <ol id="log">
-            {turns.map(turn => (
-                <li key={`${turn.square.row}${turn.square.cow}`}>
-                    {turn.player} selected {turn.square.row}, {turn.square.cow}
+            {turns.map((turn) => (
+                <li key={`${turn.square.row}${turn.square.col}`}>
+                    {turn.player} selected {turn.square.row}, {turn.square.col}
                 </li>
             ))}
         </ol>
